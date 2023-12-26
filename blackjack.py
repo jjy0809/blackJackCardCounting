61d80da94347b28763c1d005704368be8996f4ca
jjy0809 <117441526+jjy0809@users.noreply.github.com>
2023-12-26 14:11:00 +0900

    add comment

--- blackjack.py
+++ blackjack.py
@@ -3,16 +3,16 @@ import math
 import sys
 
 
-f = open('C:/Users/happy/Desktop/새 폴더 (2)/bAI.txt', 'a')
+f = open('C:/Users/happy/Desktop/새 폴더 (2)/bAI.txt', 'a') # 파일 입출력을 위한 파일 오픈(존재 하지 않을 시 생성)
 
 
-seed = 5000
-money = seed
+seed = 5000 
+money = seed # 시작할 때 가지고 있는 돈
 c_set = 1
 
-C_PER_SET = 52
+C_PER_SET = 52 # 세트 하나당 카드 수
 D_STAND = 17  # ~16 HIT
-c_arr = []
+c_arr = [] # 카드 덱을 저장할 리스트
 c_s = random.randint(math.floor(C_PER_SET*c_set/7), math.ceil(C_PER_SET*c_set/5))
 
 '''
@@ -24,10 +24,10 @@ K = 10
 CARD = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K']
 P_CARD = ['A',  'J', 'Q', 'K']
 
-m_game = 15
-game = 1
+m_game = 15 # 세트당 게임 수
+game = 1 # 몇번째 게임인지 저장하는 변수
 
-game_code = 0
+game_code = 0 # 게임 진행 사항을 알려주는 변수
 '''
 -1 Dealer Bust
 1 Player bust
@@ -43,18 +43,18 @@ hidden = 1
 win = 0
 lose = 0
 blj = 0
-ave_bet = 0
+ave_bet = 0 
 
 cm = 0
 
-count = 0
+count = 0 # 카운팅 저장 변수
 
-def dp(p):
+def dp(p): # 디버깅용 풀력 함수 (사용 X)
     #print(p)
     a2 = 0
 
 
-def shuffle():
+def shuffle(): # 덱 섞기
     global left_c
     global c_arr
     
@@ -73,8 +73,8 @@ def shuffle():
 
     dp(c_arr)
 
-def pick_c():
-    if len(c_arr) <= c_s:
+def pick_c(): # 카드 뽑기
+    if len(c_arr) <= c_s: #만약 덱에 카드가 남지 않았다면 덱을 다시 새로 섞기
         shuffle()
         print('----- Deck Shuffled -----')
         count = 0
@@ -84,7 +84,7 @@ def pick_c():
     counting(pick)
     return(c_arr[0])
 
-def sum_c(d, p):
+def sum_c(d, p): # 딜러와 플레이어의 카드 합을 리스트 형태로 반환
     dsum = 0
     da = 0
     for dc in d:
@@ -120,7 +120,7 @@ def sum_c(d, p):
     return([dsum, psum])
 
 
-def print_c(d, p):
+def print_c(d, p): # 딜러와 플레이어의 카드 합을 출력
     sc = sum_c(d, p)
 
     print("Dealer's Card: ", end='')
@@ -142,7 +142,7 @@ def print_c(d, p):
     print(' = ' + str(sc[1]))
     printCount()
 
-def result(d, p):
+def result(d, p): # 게임이 끝나면 결과를 반환
     sc = sum_c(d, p)
     if game_code == 0:
         if sc[0] > sc[1]:
@@ -174,14 +174,14 @@ def result(d, p):
     4 Surrender
     '''
 
-def line(n):
+def line(n): # 줄을 출력해주는 함수
     if n == 1:
         print('\n----------------\n')
     else:
         print('----------------')
 
 
-def counting(c):
+def counting(c): # 카운팅을 사용할 시 카드에 따라 카운팅을 자동으로 진행해주는 함수
     global count
     
     if cm == 1:
@@ -194,11 +194,11 @@ def counting(c):
         elif c == 7:
             count += 0.5
             
-def printCount():
+def printCount(): # 카운트 출력
     if cm == 1:
         print('counting: ' + str(count))
 
-def blackJack():
+def blackJack(): # 블랙잭 게임을 진행하는 함수
     dc = []
     pc = []
     
@@ -218,7 +218,7 @@ def blackJack():
     
     game_code = 0
     
-    if money <= 0:
+    if money <= 0: # 돈이 없다면 게임 종료
         print('\n\n----------------\n')
         print('no money left')
         report()
@@ -272,7 +272,7 @@ def blackJack():
         
         print('Hit: 1\nStand: 0')
         if turn == 1:
-            print('Surrender: 2\nDouble Down: 3',end=' ')
+            print('Surrender: 2\nDouble Down: 3',end=' ') # 플레이어가 취할 액션을 입력 받고 그에 따른 액션을 취하는 코드
         print('->',end=' ')
         ans = int(input())
         
@@ -309,7 +309,7 @@ def blackJack():
     line(1)
     print_c(dc, pc)
     
-    res = result(dc, pc)
+    res = result(dc, pc) # 게임 결과에 따라 결과를 출력하고 그에 맞는 액션 취함
     if res == 0:
         print('Dealer Win')
         lose += 1
@@ -330,7 +330,7 @@ def blackJack():
         money += math.floor(bet / 2)
 
 
-def report():
+def report(): # 세트가 끝나면 결과 리포트를 출력
     line(1)
     print('count: ' + str(cm))
     print('win/lose/all: ' + str(win) + ' / ' + str(lose) + ' / ' + str(m_game))
@@ -342,12 +342,12 @@ def report():
     print('income: ' + str(money - seed))
     print('income rate: ' + str((money-seed)/seed*100))
     
-    f.write(','.join([str(cm), str(win), str(lose), str(m_game), str(win/m_game*100), str(blj), str(blj/m_game*100), str(ave_bet), str(money), str(money - seed), str((money-seed)/seed*100)]))
+    f.write(','.join([str(cm), str(win), str(lose), str(m_game), str(win/m_game*100), str(blj), str(blj/m_game*100), str(ave_bet), str(money), str(money - seed), str((money-seed)/seed*100)])) # 파일입출력을 통해 결과 리포트를 텍스트파일에 저장
     f.write('\n')
     f.close()
 
 
-def main():
+def main(): # 메인함수
     global cm
     cm = int(input('count?'))
     
